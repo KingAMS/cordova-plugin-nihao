96725f6ce613c1b5b19aa73e1b565512511d3700
KingAMS <amihay.schwarz@gmail.com>
2016-03-27 10:52:22 +0300

    set com.google.android.tts

--- src/android/NH.java
+++ src/android/NH.java
@@ -35,7 +35,7 @@ public class NH extends CordovaPlugin implements OnInitListener {
 
     @Override
     public void initialize(CordovaInterface cordova, final CordovaWebView webView) {
-        tts = new TextToSpeech(cordova.getActivity().getApplicationContext(), this);
+        tts = new TextToSpeech(cordova.getActivity().getApplicationContext(), this,"com.google.android.tts");
         tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
             @Override
             public void onStart(String s) {
